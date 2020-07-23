2ad1ba25b0a8010a0f23c58cd108ad8b45a8cb52
Jeffrey Biles <bilesjeffrey@gmail.comgit config -l>
2020-07-22 17:16:08 -0700

    Install Axios, load blogs from db.json

--- src/views/Blog.vue
+++ src/views/Blog.vue
@@ -13,18 +13,19 @@
 
 <script>
   import BlogPost from '@/components/BlogPost.vue';
+  import axios from 'axios'
   export default {
     components: {
       BlogPost
     },
     data(){
       return {
-        blogs: [
-          {id: '1', title: 'Why Vue is Awesome', text: "It's easy to use, fast, and has lots of great tooling.  Plus, lots of companies will pay you to write it!"},
-          {id: '2', title: 'Best VueJS Tips', text: "1. Use a text editor 2. It's important that your computer be turned on. 3. Serious one, ask in the Slack channel if you have any questions."},
-          {id: '3', title: 'What your favorite Javascript framework says about you...', text: "Angular: You are a programmer.  React: You are a programmer.  Vue: You are a programmer, and awesome."},
-        ]
+        blogs: []
       }
+    },
+    async created(){
+      let response = await axios.get('http://localhost:3000/posts')
+      this.blogs = response.data
     }
   }
 </script>
